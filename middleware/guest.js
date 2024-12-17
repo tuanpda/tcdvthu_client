2a1e42de14c282c1a084149b52d54d8816d477ac
tuanpda <tuanpda.bhxh@gmail.com>
2024-12-17 09:24:16 +0700

    git onto phudien

--- middleware/guest.js
+++ middleware/guest.js
@@ -1,7 +1,7 @@
 export default function ({ $auth, redirect }) {
   // Kiểm tra nếu người dùng đã đăng nhập
   if ($auth.loggedIn) {
-    console.log($auth.loggedIn);
+    // console.log($auth.loggedIn);
 
     return redirect("/"); // Chuyển hướng về trang chủ hoặc trang dashboard
   }
