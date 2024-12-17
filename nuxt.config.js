3646913cae03631566f89355f14ef2866e974194
tuanpda <tuanpda.bhxh@gmail.com>
2024-12-17 08:44:42 +0700

    git onto phudien

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,14 +63,18 @@ export default {
     credentials: true,
   },
 
+  // server: {
+  //   port: 2612,
+  //   // host: "192.168.160.7", // server nhân hòa
+  //   // host: "14.224.129.177", // server tuanpda
+  //   host: "localhost",
+  //   // host: "192.168.1.97",
+  // },
+
   server: {
-    // port: 81,
-    port: 2612,
-    // host: "192.168.1.5", // tnhh phủ diễn
-    // host: "192.168.160.7", // server nhân hòa
-    // host: "14.224.129.177", // server tuanpda
-    host: "localhost",
-    // host: "192.168.1.97",
+    port: 81,
+    host: "192.168.1.5",
+    // tnhh phủ diễn
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
