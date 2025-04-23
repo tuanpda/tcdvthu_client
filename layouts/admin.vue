295807a580c49dae7e782ad49d38bb0ecb692953
tuanpda <tuanpda.bhxh@gmail.com>
2025-04-23 16:50:55 +0700

    cm

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,69 +1,80 @@
 <template>
-    <div>
-      <navbar />
-      <nuxt />
-      <Footer />
-    </div>
-  </template>
-  <script>
-  import Navbar from '~/components/admin/Navbar'
-  import Footer from "~/components/Footer"
-  export default {
-    name: "default-admin",
-    components: {
-      Navbar,
-      Footer
+  <div>
+    <navbar />
+    <nuxt />
+    <Footer />
+  </div>
+</template>
+<script>
+import Navbar from "~/components/admin/Navbar";
+import Footer from "~/components/Footer";
+export default {
+  name: "default-admin",
+  components: {
+    Navbar,
+    Footer,
+  },
+
+  async mounted() {
+    if (!this.$store.state.modules.users.user.user) {
+      try {
+        const res = await this.$axios.$get("/api/users/auth/user");
+        await this.$store.dispatch("modules/users/fetchUsersLogin", res.user);
+      } catch (e) {
+        console.error("❌ Lỗi khi lấy user từ client:", e);
+        this.$router.push("/login");
+      }
     }
-  }
-  </script>
-  
-  <style>
-  html {
-    font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-      Roboto, 'Helvetica Neue', Arial, sans-serif;
-    font-size: 16px;
-    word-spacing: 1px;
-    -ms-text-size-adjust: 100%;
-    -webkit-text-size-adjust: 100%;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-font-smoothing: antialiased;
-    box-sizing: border-box;
-  }
-  
-  *,
-  *:before,
-  *:after {
-    box-sizing: border-box;
-    margin: 0;
-  }
-  
-  .button--green {
-    display: inline-block;
-    border-radius: 4px;
-    border: 1px solid #3b8070;
-    color: #3b8070;
-    text-decoration: none;
-    padding: 10px 30px;
-  }
-  
-  .button--green:hover {
-    color: #fff;
-    background-color: #3b8070;
-  }
-  
-  .button--grey {
-    display: inline-block;
-    border-radius: 4px;
-    border: 1px solid #35495e;
-    color: #35495e;
-    text-decoration: none;
-    padding: 10px 30px;
-    margin-left: 15px;
-  }
-  
-  .button--grey:hover {
-    color: #fff;
-    background-color: #35495e;
-  }
-  </style>
-  
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
