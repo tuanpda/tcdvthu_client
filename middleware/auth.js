9e5c1d53db93e4e30150a3b03cc6aedaf95fe796
tuanpda <tuanpda.bhxh@gmail.com>
2025-04-23 14:59:41 +0700

    cm

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ import cookieparser from "cookieparser";
 
 export default async function ({ store, redirect, route, $axios, req }) {
   console.log(store.state.modules.users.user.user);
-  console.log(req.headers.cookie);
+  console.log(req);
 
   // if (!store.state.modules.users.user.user) {
   //   try {
