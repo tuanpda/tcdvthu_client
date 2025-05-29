41def7eaba8789b8cd074a9042b17913df439a59
admin.hatinh <admin.hatinh@vss.local>
2025-05-29 09:23:51 +0700

    cmi

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,10 @@ import cookieparser from "cookieparser";
 export default async function ({ store, redirect, req, app }) {
   // console.log(req);
   console.log(req.headers.cookie);
+      const parsed = cookieparser.parse(req.headers.cookie);
+    const token = parsed.token;
+    console.log(token);
+    
   
   // if (req && req.headers.cookie) {
   //   const parsed = cookieparser.parse(req.headers.cookie);
