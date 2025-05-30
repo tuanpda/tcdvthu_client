0983ed943b25c8228f761a7d1ab11a7b0af56f2c
admin.hatinh <admin.hatinh@vss.local>
2025-05-30 07:46:02 +0700

    cmi

--- middleware/auth.js
+++ middleware/auth.js
@@ -25,7 +25,9 @@ export default async function ({ store, redirect, req, app }) {
               return redirect("/tracuubienlai");
             } else {
               console.log('check index');
-              
+              const res = await app.$axios.$get("http://localhost:1552/api/users/auth/user");
+              console.log('API trả về:', res.user);
+              store.commit("setUser", res.user);  
               return redirect("/");
             }
           
