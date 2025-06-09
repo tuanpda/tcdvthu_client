22581ef1a2f4c17e266bd6d4bc2bbe7776297108
admin.hatinh <admin.hatinh@vss.local>
2025-06-09 17:04:49 +0700

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,9 +56,9 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     // baseURL: "",
-    // baseURL: "http://27.73.37.94:1552", // máy chủ phủ diễn
+    baseURL: "http://27.73.37.94:1552", // máy chủ phủ diễn
     // baseURL: "http://localhost:1552",
-    baseURL: "http://14.224.148.17:1552",
+    // baseURL: "http://14.224.148.17:1552",
     credentials: true,
   },
 
